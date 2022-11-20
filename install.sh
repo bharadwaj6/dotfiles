e999d03b3e70514b31b40444749987e44a0604c5
Bharadwaj Srigiriraju <1695154+bharadwaj6@users.noreply.github.com>
2022-11-20 22:16:30 +0000

    updates to install script for rc files

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 zshrc() {
+    echo "downloading oh-my-zsh..."
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    echo "setting up zsh auto suggestions..."
     git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+    echo "configuring zshrc..."
     cat .zshrc > "$HOME"/.zshrc
-}
+    echo "configuring rc files..."
+    cat tmux.conf > "$HOME"/.tmux.conf
+    cat vimrc > "$HOME"/.vimrc
+    cat zprofile > "$HOME"/.zprofile
+    cat zshrenv > "$HOME"/.zshenv
 
-copy_zsh_config() {
-    cp -r ./zsh ~/.zsh
+    cp -r ./zsh "$HOME"/.zsh
 }
 
 zshrc
-copy_zsh_config
